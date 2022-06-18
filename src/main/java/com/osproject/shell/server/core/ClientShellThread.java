e6db5054ae3fac2ebc98f7a02d04528401bb0c0f
Kevin Martinez <ksmartinez2307@gmail.com>
2022-06-18 15:56:36 -0500

    Update ClientShellThread.java

--- src/main/java/com/osproject/shell/server/core/ClientShellThread.java
+++ src/main/java/com/osproject/shell/server/core/ClientShellThread.java
@@ -7,16 +7,17 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.InetAddress;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ClientShellThread extends Thread{
+public class ClientShellThread extends Thread {
 
     private final Socket socket;
     private final DataInputStream dataInputStream;
     private final DataOutputStream dataOutputStream;
 
-    public ClientShellThread(Socket socket, DataInputStream dataInputStream, DataOutputStream dataOutputStream){
+    public ClientShellThread(Socket socket, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
         this.socket = socket;
         this.dataInputStream = dataInputStream;
         this.dataOutputStream = dataOutputStream;
@@ -27,16 +28,16 @@ public class ClientShellThread extends Thread{
         String[] request;
         boolean exit = false;
         Shell shell = new Shell();
-        while(!exit) {
+        InetAddress clientIp = socket.getInetAddress();
+        do {
             try {
                 request = dataInputStream.readUTF().split("::");
 
                 switch (request[0]) {
                     case "0" -> {
+                        System.out.println(ConsoleColors.BLUE + "server@linux~$ " + ConsoleColors.YELLOW + "Closing connection with " + ConsoleColors.WHITE + clientIp + ConsoleColors.YELLOW + "..." + ConsoleColors.RESET);
                         exit = true;
                         this.socket.close();
-                        System.out.println(ConsoleColors.BLUE + "server@linux~$ " + ConsoleColors.YELLOW + "Closing connection with " + ConsoleColors.WHITE + socket.getInetAddress() + ConsoleColors.YELLOW + "..." + ConsoleColors.RESET);
-                        System.out.println(ConsoleColors.BLUE + "server@linux~$ " + ConsoleColors.RED + "Connection closed" + ConsoleColors.RESET);
                     }
                     case "1" -> {
                         boolean login = new Auth().auth(request[1]);
@@ -47,16 +48,24 @@ public class ClientShellThread extends Thread{
                             System.out.println(ConsoleColors.BLUE + "server@linux~$ " + ConsoleColors.GREEN + "Connection request from " + ConsoleColors.WHITE + socket.getInetAddress() + ConsoleColors.GREEN + " accepted" + ConsoleColors.RESET);
                         }
                     }
-                    case "2" -> dataOutputStream.writeUTF(shell.execute(request[1]));
-                    case "3" -> dataOutputStream.writeUTF(shell.execute("free -m"));
-                    case "4" -> dataOutputStream.writeUTF(shell.execute("df -h"));
-                    case "5" -> dataOutputStream.writeUTF(shell.execute("top -b -n 1"));
+                    case "2" ->
+                        dataOutputStream.writeUTF(shell.execute(request[1]));
+                    case "3" ->
+                        dataOutputStream.writeUTF(shell.execute("free -m"));
+                    case "4" ->
+                        dataOutputStream.writeUTF(shell.execute("df -h"));
+                    case "5" ->
+                        dataOutputStream.writeUTF(shell.execute("wk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) \"%\"; }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat)"));
+                    case "6" ->
+                        dataOutputStream.writeUTF(shell.execute("top -b -n 1"));
                 }
 
             } catch (IOException ex) {
+                exit = true;
                 Logger.getLogger(ShellServer.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }
+        } while (!exit);
+        System.out.println(ConsoleColors.BLUE + "server@linux~$ " + ConsoleColors.RED + "Connection with " + ConsoleColors.WHITE + clientIp + ConsoleColors.RED + " closed" + ConsoleColors.RESET);
         try {
             this.dataOutputStream.close();
             this.dataInputStream.close();
